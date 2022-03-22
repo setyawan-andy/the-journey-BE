da37c788164d880bca7d9059cf007ae4f498d403
setyawan-andy <101380268+setyawan-andy@users.noreply.github.com>
2022-03-22 13:50:23 +0700

    Update 20220322063048-user.js

--- seeders/20220322063048-user.js
+++ seeders/20220322063048-user.js
@@ -18,7 +18,9 @@ module.exports = {
          fullName: "user1",
          email: "user1@email.com",
          password: "$2b$10$UNmEUz5PvWv1Mr1.4TFG6evQCEsC.lAjf94Kva6R1.nwPhERAlane", //abc123
-         phone: "12345678910"
+         phone: "12345678910",
+         createdAt: 2022-03-15 03:57:09,
+         updatedAt: 2022-03-15 03:57:09,
        },
      ],
      {}
