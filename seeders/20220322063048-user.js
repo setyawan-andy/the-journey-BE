07f11bfde35c4455f9915ae5d286d288340d41d3
setyawan-andy <101380268+setyawan-andy@users.noreply.github.com>
2022-03-22 13:42:36 +0700

    Add files via upload

---
+++ seeders/20220322063048-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert(
+     "users",
+     [
+       {
+         fullName: "user1",
+         email: "user1@email.com",
+         password: "$2b$10$UNmEUz5PvWv1Mr1.4TFG6evQCEsC.lAjf94Kva6R1.nwPhERAlane", //abc123
+         phone: "12345678910"
+       },
+     ],
+     {}
+   );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
